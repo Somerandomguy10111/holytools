2bf75cf01c32c8735bb739a74959a68c1cc55f16
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-12 08:52:13 +0100

    Depdendencies: Cleaned generate venv

--- dependencies/generate_venv.py
+++ dependencies/generate_venv.py
@@ -1,16 +1,30 @@
+import subprocess
+import sys
+import os
 
 
-def generate_venv(foldername : str):
-    import subprocess
-    import sys
-    import os
-
+def generate_venv(src_dirpath : str, venv_name : str = 'venv'):
     # setup dependencies
-    subprocess.run([sys.executable, "-m", "dependencies", "dependencies"], check=True)
-    venv_python = os.path.join("dependencies", "bin", "python")
-    subprocess.run([venv_python, "-m", "pip", "install", "pipreqs"], check=True)
+    """
+    Generates a venv with all python packages that can be found in src code in src_dirpath
+    """
+    # mk venv
+
+    run = lambda cmd : subprocess.run(cmd, check=True)
+
+    print(f'-> Creating virtual environment')
+    venv_path = os.path.join(venv_name, "bin", "python")
+    run([sys.executable, "-m", "venv", venv_name])
 
     # Install generate requirements and install
-    subprocess.run(["pipreqs", foldername], check=True)
-    command = f"source dependencies/bin/activate && pip install -r {foldername}/requirements.txt"
-    subprocess.run(command, shell=True, executable='/bin/bash', check=True)
+    print(f'-> Generating requirements.txt')
+    run([venv_path, "-m", "pip", "install", "pipreqs"])
+    run(["pipreqs", src_dirpath])
+
+    print(f'-> Installing requirements')
+    command = f"source {venv_name}/bin/activate && pip install -r {src_dirpath}/requirements.txt"
+    subprocess.run(command, shell=True, executable='/bin/bash')
+
+
+if __name__ == "__main__":
+    generate_venv(src_dirpath=os.getcwd())
