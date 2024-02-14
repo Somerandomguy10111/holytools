b400e207a7d8e5f5296d9ba5efe889da08ce6b08
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-15 00:39:44 +0100

    io/fsys: Updated FsysNode

--- hollarek/io/fsys/fsys_node.py
+++ hollarek/io/fsys/fsys_node.py
@@ -5,12 +5,15 @@ import os
 
 # -------------------------------------------
 
-
 class FsysNode:
     def __init__(self, path : str):
-        self.path : str = path
-        self.name : str = os.path.basename(path)
+        self._path : str = path
         self._subnodes : Optional[list[FsysNode]] = None
+        if not self.get_is_resource():
+            raise FileNotFoundError(f'Path {path} is not a file/folder')
+
+    # -------------------------------------------
+    # sub
 
     def select_file_subnodes(self, allowed_formats : list[str]) -> list[FsysNode]:
         selected_node = []
@@ -25,34 +28,40 @@ class FsysNode:
     def get_file_subnodes(self) -> list[FsysNode]:
         return [des for des in self.get_subnodes() if des.get_is_file()]
 
-    def get_subnodes(self):
+
+    def get_subnodes(self) -> list[FsysNode]:
         if self._subnodes is None:
-            path_list = list(Path(self.path).rglob('*'))
+            path_list = list(Path(self._path).rglob('*'))
             self._subnodes: list[FsysNode] = [FsysNode(str(path)) for path in path_list]
         return self._subnodes
 
+    # -------------------------------------------
+    # resource info
+
     def get_suffix(self) -> Optional[str]:
         try:
-            suffix = self.name.split('.')[-1]
+            suffix = self.get_name().split('.')[-1]
         except:
             suffix = None
         return suffix
 
     def get_epochtime_last_modified(self) -> float:
-        return os.path.getmtime(self.path)
+        return os.path.getmtime(self._path)
 
     def get_size_in_MB(self) -> float:
-        return os.path.getsize(self.path) / (1024 * 1024)
+        return os.path.getsize(self._path) / (1024 * 1024)
 
-    def get_is_resources(self) -> bool:
+    def get_name(self) -> str:
+        return os.path.basename(self._path)
+
+    def get_is_resource(self) -> bool:
         return self.get_is_dir() or self.get_is_file()
 
     def get_is_file(self) -> bool:
-        return os.path.isfile(self.path)
+        return os.path.isfile(self._path)
 
     def get_is_dir(self) -> bool:
-        return os.path.isdir(self.path)
-
+        return os.path.isdir(self._path)
 
 
 if __name__ == "__main__":
