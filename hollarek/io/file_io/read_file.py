4bcd3f31d68558d779b5cd8b248213ee6905c2be
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-15 00:53:25 +0100

    io/fileio: Updated read_file to use Enums

--- hollarek/io/file_io/read_file.py
+++ hollarek/io/file_io/read_file.py
@@ -1,17 +1,25 @@
 from pypdf import PdfReader
 from enum import Enum
 
-class FileTypes(Enum):
+class FileType(Enum):
     TXT = 'TXT'
     PDF = 'PDF'
 
-def get_txt_file_content(file_path : str) -> str:
+
+def get_text_file_content(fpath: str, file_type : FileType) -> str:
+    if file_type == FileType.TXT:
+        return _get_plain_text_content(file_path=fpath)
+    elif file_type == FileType.PDF:
+        return _get_pdf_file_content(file_path=fpath)
+
+
+def _get_plain_text_content(file_path : str) -> str:
     with open(file_path, 'r') as file:
         file_content = file.read()
     return file_content
 
 
-def get_pdf_file_content(file_path : str) -> str:
+def _get_pdf_file_content(file_path : str) -> str:
     pdf_file = open(file_path, 'rb')
     pdf_reader = PdfReader(pdf_file)
 
