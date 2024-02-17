71cd87f825c663ce04ee0eace77cce8254b0be8d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-17 14:57:01 +0100

    ext_logging: Added timestamp test

--- hollarek/dev/ext_logging/get_timestamp.py
+++ hollarek/dev/ext_logging/get_timestamp.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
 def get_timestamp(use_subscores : bool = True, date_only : bool = False) -> str:
-    fomat_str = '%Y-%m-d'
+    fomat_str = '%Y-%m-%d'
 
     if not date_only:
         time_of_day_str = ' %H_%M_%S' if use_subscores else ' %H:%M:%S'
         fomat_str += time_of_day_str
 
     return datetime.now().strftime(fomat_str)
+
+if __name__ == "__main__":
+    tf = [True, False]
+    for (_use_subscore, _date_only) in [(x, y) for x in tf for y in tf]:
+        print(f"use_subscore, date_onyl = {_use_subscore}, {_date_only}: timestamp: {get_timestamp(_use_subscore, _date_only)}")
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt','r') as file:
 
 setup(
     name='hollarek',
-    version='0.5.2',
+    version='0.5.3',
     author='Daniel Hollarek',
     author_email='daniel.hollarek@googlemail.com',
     description='Collection of general python utilities for future projects',
