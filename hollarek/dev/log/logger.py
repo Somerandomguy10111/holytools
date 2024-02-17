bd70f90af05e680070ec1ecbe80f2a2553ccc498
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-17 20:51:57 +0100

    dev/log: Included option to print call location

--- hollarek/dev/log/logger.py
+++ hollarek/dev/log/logger.py
@@ -87,21 +87,24 @@ class ColoredFormatter(logging.Formatter):
     }
 
     def __init__(self, use_timestamp: bool = True,
-                 include_ms_in_timestamp : bool = False):
+                       include_ms_in_timestamp : bool = False,
+                       include_call_location : bool = True):
         super().__init__()
         self.print_timestamp : bool = use_timestamp
         self.include_ms_in_timestamp : bool = include_ms_in_timestamp
+        self.include_call_location : bool = include_call_location
 
     def format(self, record):
-        base_log_fmt = "%(message)s"
+        log_fmt = "%(message)s"
 
         if self.print_timestamp:
             custom_time = self.formatTime(record, "%Y-%m-%d %H:%M:%S")
             conditional_millis = f"{int(record.msecs)}ms" if self.include_ms_in_timestamp else ""
             timestamp = f"[{custom_time}{conditional_millis}]"
-            log_fmt = f"{timestamp}: {base_log_fmt}"
-        else:
-            log_fmt = base_log_fmt
+            log_fmt = f"{timestamp}: {log_fmt}"
+
+        if self.include_call_location:
+            log_fmt = f"{log_fmt} (%(filename)s:%(lineno)d)"
 
         color_prefix = ColoredFormatter.colors.get(LogLevel(record.levelno), "")
         color_suffix = "\033[0m"
@@ -110,8 +113,6 @@ class ColoredFormatter(logging.Formatter):
         self._style._fmt = log_fmt
         return super().format(record)
 
-        # if LogSettings._instance._default_logfile_path:
-        #     log_fmt = f"{log_fmt} (%(filename)s:%(lineno)d)"
 
 def log(msg : str,
         log_level: LogLevel = LogLevel.INFO,
