b4b2f7dbe4d27b90d856a507af62d9b3d6381c7e
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-19 14:40:30 +0100

    dev/harddware: Rebuild through Partition class

--- hollarek/dev/hardware/drives.py
+++ hollarek/dev/hardware/drives.py
@@ -1,5 +1,5 @@
-import os
-import tempfile, shutil
+from typing import Optional
+import shutil, os
 
 from hollarek.dev.log import get_logger, LogSettings
 import logging
@@ -11,32 +11,75 @@ def log(msg : str, level : int = logging.INFO):
     drive_logger.log(msg=msg, level=level)
 
 
-class Drive:
-    pass
+class Partition:
+    @classmethod
+    def from_resource_path(cls, resource_path : str):
+        device_name = find_device_of_path(resource_path=resource_path)
+        return cls(device_name=device_name)
 
-def get_free_space_in_GB(directory : str) -> float:
-    total, used, free = shutil.disk_usage(os.path.realpath(directory))
-    return free / (1024**3)
+    def __init__(self, device_name: str):
+        self.mount_point : str = get_device_mount_point(device_name=device_name)
+        if not self.mount_point:
+            raise ResourceWarning(f'Could not find mount point for device {device_name}')
 
-def get_total_space_in_GB(directory : str) -> float:
-    total, used, free = shutil.disk_usage(os.path.realpath(directory))
-    return total / (1024**3)
+        self.device_name : str = device_name
 
 
-def print_free_space_info(location_path : str):
-    warning_free_space = 250
-    critical_free_space_in_GB = 100
+    def print_free_space_info(self):
+        warning_free_space = 250
+        critical_free_space_in_GB = 100
 
-    free_space_GB = int(round(get_free_space_in_GB(directory=location_path),0))
-    total_space_GB = int(round(get_total_space_in_GB(directory=location_path), 0))
-    log(f'Free space at {location_path}: {free_space_GB}/{total_space_GB} GB')
+        free_space_GB = int(round(self.get_free_space_in_GB(), 0))
+        total_space_GB = int(round(self.get_total_space_in_GB(), 0))
+        log(f'Free space on partition {self.device_name}: {free_space_GB}/{total_space_GB} GB')
 
-    if free_space_GB < warning_free_space:
-        log(f'Warning: Disk space is running low at "{location_path}. Only {free_space_GB} GB left!', level=logging.WARNING)
+        if free_space_GB < critical_free_space_in_GB:
+            log(f'Warning: Almost no disk space remaining on {self.device_name}. Only {free_space_GB} GB left!')
+        elif free_space_GB < warning_free_space:
+            log(f'Warning: Disk space is running low on "{self.device_name}". Only {free_space_GB} GB left!')
 
-    elif free_space_GB < critical_free_space_in_GB:
-        log(f'Warning: Almost no disk space remaining at {location_path}. Only {free_space_GB} GB left!', level=logging.CRITICAL)
+
+    def get_free_space_in_GB(self) -> float:
+        stats = psutil.disk_usage(self.mount_point)
+        return stats.free / (1024 ** 3)
+
+    def get_total_space_in_GB(self) -> float:
+        stats = psutil.disk_usage(self.mount_point)
+        return stats.total / (1024 ** 3)
+
+def find_device_of_path(resource_path: str) -> Optional[str]:
+    absolute_path = os.path.abspath(resource_path)
+
+    partitions = psutil.disk_partitions(all=True)
+    partitions.sort(key=lambda x: len(x.mountpoint), reverse=True)
+
+    for partition in partitions:
+        if absolute_path.startswith(partition.mountpoint):
+            return partition.device
+
+    return None
+
+
+def get_device_mount_point(device_name : str) -> Optional[str]:
+    partitions = psutil.disk_partitions()
+    for part in partitions:
+        if part.device == device_name:
+            return part.mountpoint
+    return None
 
 
 if __name__ == '__main__':
-    print_free_space_info(location_path='/home/daniel/OneDrive')
+    import psutil
+    # test_partitions = psutil.disk_partitions()
+    # print(test_partitions)
+
+    test_part = Partition(device_name='/dev/nvme0n1p2')
+    log(test_part.mount_point)
+    test_part.print_free_space_info()
+
+    new_part = Partition.from_resource_path(resource_path='/media/daniel/STICKY1')
+    log(new_part.mount_point)
+    new_part.print_free_space_info()
+
+    # for p in partitions:
+    #     print(p.mountpoint, psutil.disk_usage(p.mountpoint).percent)
