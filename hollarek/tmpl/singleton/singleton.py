7b00d88fc9b9fe81a974625fa46568c4dfe37060
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-24 23:11:59 +0100

    Singleton: Introduced option to reset instance

--- hollarek/tmpl/singleton/singleton.py
+++ hollarek/tmpl/singleton/singleton.py
@@ -5,6 +5,12 @@ class Singleton(Loggable):
     _instance = None
     is_initialized = False
 
+    @classmethod
+    def reset_instance(cls):
+        cls._instance = None
+        cls.is_initialized = False
+        
+
     def __new__(cls, *args, **kwargs):
         if (args or kwargs) and cls.is_initialized:
             cls.cls_log("Warning: Additional arguments provided to an already initialized singleton",
