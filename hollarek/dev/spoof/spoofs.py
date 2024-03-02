2a984b749710c9bd26f2bfaa9ca0c7a28c2fccab
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-02 03:00:33 +0100

    dev: Updated spoof module

--- hollarek/dev/spoof/spoofs.py
+++ hollarek/dev/spoof/spoofs.py
@@ -1,33 +1,49 @@
 import os
-def load_asset_as_bytes(filename: str) -> bytes:
+import shutil
+import tempfile
+
+
+def create_temp_copy(filename: str) -> str:
     module_dir = os.path.dirname(__file__)
-    file_path = os.path.join(module_dir, filename)
+    original_file_path = os.path.join(module_dir, filename)
+
     try:
-        with open(file_path, 'rb') as file:
-            return file.read()
+        temp_fd, temp_filepath = tempfile.mkstemp(suffix=os.path.splitext(filename)[1], dir='/tmp')
+        os.close(temp_fd)
+        shutil.copy2(original_file_path, temp_filepath)
+
+        return temp_filepath
     except FileNotFoundError:
         print(f"File {filename} not found.")
-        return b''  # Return empty bytes if the file does not exist
+        return ''
 
-def get_test_png() -> bytes:
-    return load_asset_as_bytes('test.png')
 
-def get_test_jpg() -> bytes:
-    return load_asset_as_bytes('test.jpg')
+def get_temp_test_png() -> str:
+    return create_temp_copy('test.png')
 
-def get_test_pdf() -> bytes:
-    return load_asset_as_bytes('test.pdf')
+def get_temp_test_jpg() -> str:
+    return create_temp_copy('test.jpg')
 
-def get_test_txt() -> bytes:
-    return load_asset_as_bytes('test.txt')
+def get_temp_test_pdf() -> str:
+    return create_temp_copy('test.pdf')
 
-def get_test_csv() -> bytes:
-    return load_asset_as_bytes('test.csv')
+def get_temp_test_txt() -> str:
+    return create_temp_copy('test.txt')
+
+def get_temp_test_csv() -> str:
+    return create_temp_copy('test.csv')
 
 
 if __name__ == "__main__":
-    png_content = get_test_png()
-    jpg_content = get_test_jpg()
-    pdf_content = get_test_pdf()
-    txt_content = get_test_txt()
-    csv_content = get_test_csv()
+    temp_png_path = get_temp_test_png()
+    temp_jpg_path = get_temp_test_jpg()
+    temp_pdf_path = get_temp_test_pdf()
+    temp_txt_path = get_temp_test_txt()
+    temp_csv_path = get_temp_test_csv()
+
+    # Print the paths of the temporary files
+    print(f"Temporary PNG file created at: {temp_png_path}")
+    print(f"Temporary JPG file created at: {temp_jpg_path}")
+    print(f"Temporary PDF file created at: {temp_pdf_path}")
+    print(f"Temporary TXT file created at: {temp_txt_path}")
+    print(f"Temporary CSV file created at: {temp_csv_path}")
