d9e142db9352c697bdad8cbd60fb0ffebd337b6f
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-02 01:52:13 +0100

    events: Fixed scheduler init

--- hollarek/events/schedule.py
+++ hollarek/events/schedule.py
@@ -1,23 +1,21 @@
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
+from hollarek.tmpl.singleton import Singleton
+from typing import Optional
 
-logging.getLogger('apscheduler').setLevel(logging.ERROR)
-scheduler = BackgroundScheduler()
+
+class ScheduleHandler(Singleton):
+    scheduler : Optional[BackgroundScheduler]= None
+    logging.getLogger('apscheduler').setLevel(logging.ERROR)
+
+    @classmethod
+    def get_scheduler(cls):
+        if not cls.scheduler:
+            cls.scheduler = BackgroundScheduler()
+            cls.scheduler.start()
+        return cls.scheduler
+
 
 def schedule(callback: callable, interval_in_sec: int):
+    scheduler = ScheduleHandler.get_scheduler()
     scheduler.add_job(callback, 'interval', seconds=interval_in_sec)
-    if not scheduler.running:
-        scheduler.start()
-
-
-def enable_schedule_logging():
-    set_schedule_logging_level(level=logging.INFO)
-
-
-def disable_schedule_logging():
-    set_schedule_logging_level(level=logging.CRITICAL)
-
-
-def set_schedule_logging_level(level):
-    logging.getLogger('apscheduler').setLevel(level)
-
