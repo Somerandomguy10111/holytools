bbbe81017a06108d331cde4387b45b84e1b8eff9
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-03 00:13:31 +0100

    template: Updated tree node template

--- hollarek/templates/tree/node.py
+++ hollarek/templates/tree/node.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Optional
+from abc import abstractmethod
 # -------------------------------------------
 
 
@@ -7,16 +8,19 @@ class TreeNode:
     def __init__(self, name : str, parent : Optional[TreeNode] = None):
         self._name : str = name if parent else '/'
         self._parent : TreeNode = parent
-        self._children : set[TreeNode] = set()
+        self._children : Optional[set[TreeNode]] = set()
 
     def remove_child(self, node : TreeNode):
         self._children.remove(node)
-
 
     def _add_child(self, node : TreeNode):
         if node.get_parent() != self:
             raise ValueError(f'Node already has parent')
         self._children.add(node)
+
+    @abstractmethod
+    def _retrieve_children(self) -> set[TreeNode]:
+        pass
 
     # -------------------------------------------
     # get
@@ -25,15 +29,26 @@ class TreeNode:
         return self._name
 
 
-    def get_tree(self) -> dict:
-        tree = {}
-        for child in self._children:
-            tree[child] = child.get_tree()
-        return tree
+    def get_ancestors(self) -> list[TreeNode]:
+        current = self
+        ancestors = []
+        while current._parent:
+            ancestors.append(current._parent)
+            current = current._parent
+        return ancestors
 
 
-    def get_children(self) -> list[TreeNode]:
-        return list(self._children)
+    def get_child_nodes(self) -> set[TreeNode]:
+        if self._children is None:
+            self._children = self._retrieve_children()
+        return self._children
+
+
+    def get_dict(self) -> Optional[dict]:
+        if not self.get_child_nodes():
+            return None
+
+        return {child.get_name() : child.get_dict() for child in self.get_child_nodes()}
 
 
     def get_root(self) -> TreeNode:
@@ -41,6 +56,7 @@ class TreeNode:
         while current._parent:
             current = current._parent
         return current
+
 
     def get_parent(self) -> Optional[TreeNode]:
         return self._parent
