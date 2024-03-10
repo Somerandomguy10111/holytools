ec94457ae76b6f2e06c240638fb25ef3568f3967
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-10 16:53:08 +0100

    events: Introduced optional target value in input_waiter

--- hollarek/events/input_waiter.py
+++ hollarek/events/input_waiter.py
@@ -1,9 +1,16 @@
+from typing import Any
 from queue import Queue
 
 
+
+
 class InputWaiter:
-    def __init__(self):
+    _undefined = object()
+
+    def __init__(self, target_value : Any = _undefined):
         self.q = Queue()
+        self.target_value : Any = target_value
+        self.input_found : bool = False
 
     def clear(self):
         self.q = Queue()
@@ -11,5 +18,15 @@ class InputWaiter:
     def write(self, value):
         self.q.put(value)
 
-    def read(self):
-        return self.q.get()
+    def read(self) -> Any:
+        value = self.q.get()
+        if not self.target_value == InputWaiter._undefined:
+            if value == self.target_value:
+                self.input_found = True
+        else:
+            self.input_found = True
+        return value
+
+
+if __name__ == "__main__":
+    waiter = InputWaiter(target_value=None)
--- hollarek/hardware/input/keyboard.py
+++ hollarek/hardware/input/keyboard.py
@@ -1,5 +1,9 @@
 import pyautogui
 import time
+from hollarek.events import InputWaiter
+from pynput import keyboard
+import time
+from pynput.keyboard import Key
 
 class Keyboard:
     @staticmethod
@@ -11,3 +15,64 @@ if __name__ == "__main__":
     print(f'Will now type something')
     time.sleep(2)
     Keyboard.type("Hello, this is a test typing.")
+
+
+
+class KeyboardListener:
+    def __init__(self):
+        self.listener = keyboard.Listener(on_press=self._add_pressed, on_release=self._remove_pressed)
+        self.pressed_buttons : set[Key] = set()
+        self.waiters : list[InputWaiter] = []
+
+
+    def wait_on_press(self, key : Key):
+        self.register_waiter()
+
+
+
+
+    # def wait_hold(self, key : Key):
+
+
+    def get_next_key(self) -> Key:
+        waiter = InputWaiter()
+        self.waiters.append(waiter)
+        return waiter.read()
+
+    # ---------------------------------------------------------
+
+    def _add_pressed(self, key: Key):
+        for waiter in self.waiters:
+            waiter.write(key)
+        self.waiters = []
+        self.pressed_buttons.add(key)
+
+
+    def _remove_pressed(self, key : Key):
+        if key in self.pressed_buttons:
+            self.pressed_buttons.remove(key)
+
+    def register_waiter(self, target_value : Key) -> InputWaiter:
+        waiter = InputWaiter(target_value)
+        self.waiters.append(waiter)
+        return waiter
+
+#     def on_press(self, key):
+#         if key == keyboard.Key.esc:
+#             self.start_time = time.time()  # Record the time when Esc is pressed
+#             print(f'Esc pressed at {self.start_time}')
+#
+#     def on_release(self, key):
+#         if key == keyboard.Key.esc and self.start_time:
+#             elapsed_time = time.time() - self.start_time
+#             if elapsed_time >= 1:  # Check if Esc was held for at least 1 second
+#                 return False
+#             self.start_time = None  # Reset start time if Esc was not held long enough
+#
+# def wait_for_esc_pressed_for_1_sec():
+#     listener = KeyboardListener()
+#     with keyboard.Listener(on_press=listener.on_press) as listener:
+#         listener.join()
+#
+# # Call the function
+# wait_for_esc_pressed_for_1_sec()
