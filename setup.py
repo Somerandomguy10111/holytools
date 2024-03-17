620f8e13364384ef014a886621e8381e8b7ef1bc
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-17 22:40:02 +0100

    General: Updated spoof data location

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 
-
 with open('requirements.txt','r') as file:
     data = file.read()
     requirements = data.split()
@@ -14,7 +13,7 @@ setup(
     description='Collection of general python utilities for future projects',
     url='https://github.com/Somerandomguy10111/hollarek',
     packages=find_packages(),
-    package_data={'hollarek.devtools.spoof': ['*']},
+    package_data={'hollarek.file.spoof': ['*']},
     python_requires='>=3.8',
     include_package_data=True,
     install_requires=requirements
