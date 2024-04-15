2d7d5439504b5f2bb5b6e0225d7caa2d2eac75aa
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-15 18:13:03 +0200

    tests: Updated unittest test

--- tests/t_devtools/t_unit.py
+++ tests/t_devtools/t_unit.py
@@ -5,7 +5,7 @@ from tests.t_devtools.t_dtyping import TestOptionalTyping
 class TestMeta(Unittest):
     def test_summary(self):
         results = TestOptionalTyping.execute_all()
-        self.assertIn(f'4/4 tests ran successfully!',results.get_final_status())
+        self.assertIn(f'tests ran successfully!',results.get_final_status())
 
 if __name__ == "__main__":
     TestMeta.execute_all()
