bae11efc82c3e33cf2d1d2fcce8886e53cd39d45
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-17 06:19:46 +0200

    tests: Added logger retrievable test

--- tests/t_logging.py
+++ tests/t_logging.py
@@ -1,5 +1,12 @@
-from hollarek.core.logging import LogLevel
+import logging
+from hollarek.core.logging import LogLevel, get_logger
 from hollarek.devtools import Unittest
+# from PIL import Image
+
+
+logger_name = "basic_xyz_logger"
+_ = get_logger(name=logger_name)
+
 
 class TestLoggin(Unittest):
     def test_info_error(self):
@@ -7,6 +14,13 @@ class TestLoggin(Unittest):
         self.log(f'Error text', level=LogLevel.ERROR)
 
 
+    def test_get_logger(self):
+        retrieve = logging.getLogger(logger_name)
+        self.assertIsNotNone(retrieve)
+        logger_is_registered = logger_name in logging.root.manager.loggerDict
+
+        print(f'{logging.root.manager.loggerDict}')
+        self.assertTrue(logger_is_registered)
 
 if __name__ == '__main__':
     TestLoggin.execute_all()
