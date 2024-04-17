570383f2112e5de2c323eb0f0b3e8966225f7f82
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-17 06:42:50 +0200

    tests: fixed bug in test_get_logger that would make test always pass

--- tests/t_logging.py
+++ tests/t_logging.py
@@ -4,8 +4,6 @@ from hollarek.devtools import Unittest
 # from PIL import Image
 
 
-logger_name = "basic_xyz_logger"
-_ = get_logger(name=logger_name)
 
 
 class TestLoggin(Unittest):
@@ -13,14 +11,13 @@ class TestLoggin(Unittest):
         self.log(f'Info text', level=LogLevel.INFO)
         self.log(f'Error text', level=LogLevel.ERROR)
 
-
     def test_get_logger(self):
-        retrieve = logging.getLogger(logger_name)
-        self.assertIsNotNone(retrieve)
-        logger_is_registered = logger_name in logging.root.manager.loggerDict
+        logger_name = "basic_xyz_logger"
+        _ = get_logger(name=logger_name)
 
-        print(f'{logging.root.manager.loggerDict}')
+        logger_is_registered = logger_name in logging.root.manager.loggerDict
         self.assertTrue(logger_is_registered)
+
 
 if __name__ == '__main__':
     TestLoggin.execute_all()
