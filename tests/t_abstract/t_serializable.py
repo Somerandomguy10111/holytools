bde2ddd93e5033e8e9fce7f138a9163f176a3cf6
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-21 09:41:06 +0200

    tests: Introducing hiding of Serialization base test to prevent its discovery

--- tests/t_abstract/t_serializable.py
+++ tests/t_abstract/t_serializable.py
@@ -9,7 +9,7 @@ from hollarek.abstract import SerializableType, JsonDataclass
 from hollarek.devtools import Unittest
 
 
-class MyEnum(Enum):
+class ThisParticularEnum(Enum):
     OPTION_A = 1
     OPTION_B = 2
 
@@ -33,81 +33,82 @@ simple_data_instance = SimpleDataclass(
     unique_id=UUID('12345678-1234-5678-1234-567812345678')
 )
 
+# This is to prevent discovery of the base test
+class HiderClass:
+    class SerializationBase(Unittest):
+        def setUp(self):
+            self.instance, self.cls = self.get_instance_and_cls()
 
-class SerializationBaseTest(Unittest):
-    def setUp(self):
-        self.instance, self.cls = self.get_instance_and_cls()
-
-    def get_instance_and_cls(self) -> (SerializableType, type[SerializableType]):
-        test_date = date.today()
-        test_time = time(12, 34, 56)
-
-
-        ClassType = self.get_serializable()
-
-        @dataclass
-        class ComplexDataclass(ClassType):
-            date_field: date
-            time_field: time
-            enum_field : MyEnum
-            simple_data: SimpleDataclass
-            dictionary_data: dict[str, str] = field(default_factory=dict)
-
-            def __post_init__(self):
-                self.dictionary_data = {'key1': 'value1', 'key2': 'value2'}
+        def get_instance_and_cls(self) -> (SerializableType, type[SerializableType]):
+            test_date = date.today()
+            test_time = time(12, 34, 56)
 
 
+            ClassType = self.get_serializable()
 
-        
-        instance = ComplexDataclass(
-            date_field=test_date,
-            time_field=test_time,
-            enum_field=MyEnum.OPTION_A,
-            simple_data=simple_data_instance
-        )
+            @dataclass
+            class ComplexDataclass(ClassType):
+                date_field: date
+                time_field: time
+                enum_field : ThisParticularEnum
+                simple_data: SimpleDataclass
+                dictionary_data: dict[str, str] = field(default_factory=dict)
 
-        return instance, ComplexDataclass
+                def __post_init__(self):
+                    self.dictionary_data = {'key1': 'value1', 'key2': 'value2'}
 
+            instance = ComplexDataclass(
+                date_field=test_date,
+                time_field=test_time,
+                enum_field=ThisParticularEnum.OPTION_A,
+                simple_data=simple_data_instance
+            )
 
-    def test_serialization_roundtrip(self):
-        serialized_str = self.instance.to_str()
-        reloaded_data = self.cls.from_str(serialized_str)
-        self.check_effectively_equal(obj1=self.instance, obj2=reloaded_data)
-
-    def test_save_load_roundtrip(self):
-        with NamedTemporaryFile(delete=False) as temp_file:
-            temp_file_path = temp_file.name
-        self.instance.save(temp_file_path, force_overwrite=True)
-        reloaded_data = self.cls.load(temp_file_path)
-
-        self.check_effectively_equal(obj1=self.instance, obj2=reloaded_data)
-
-    def check_effectively_equal(self, obj1 : object, obj2 : object):
-        original_dict = str(obj1.__dict__)
-        reloaded_dict = str(obj2.__dict__)
-        self.assertEqual(original_dict, reloaded_dict)
+            return instance, ComplexDataclass
 
 
-    @classmethod
-    @abstractmethod
-    def get_serializable(cls) -> SerializableType:
-        pass
+        def test_serialization_roundtrip(self):
+            serialized_str = self.instance.to_str()
+            reloaded_data = self.cls.from_str(serialized_str)
+            self.check_effectively_equal(obj1=self.instance, obj2=reloaded_data)
+
+        def test_save_load_roundtrip(self):
+            with NamedTemporaryFile(delete=False) as temp_file:
+                temp_file_path = temp_file.name
+            self.instance.save(temp_file_path, force_overwrite=True)
+            reloaded_data = self.cls.load(temp_file_path)
+
+            self.check_effectively_equal(obj1=self.instance, obj2=reloaded_data)
+
+        def check_effectively_equal(self, obj1 : object, obj2 : object):
+            original_dict = str(obj1.__dict__)
+            reloaded_dict = str(obj2.__dict__)
+            self.assertEqual(original_dict, reloaded_dict)
+            print()
+            print(f'{original_dict}, {reloaded_dict} for {self.__class__.__name__}')
 
 
-class TestJsonDataclass(SerializationBaseTest):
+        @classmethod
+        @abstractmethod
+        def get_serializable(cls) -> SerializableType:
+            pass
+
+
+class TestJsonDataclass(HiderClass.SerializationBase):
     @classmethod
     def get_serializable(cls):
         from hollarek.abstract.serialization import JsonDataclass
         return JsonDataclass
 
 
-class TestDillable(SerializationBaseTest):
+class TestDillable(HiderClass.SerializationBase):
     @classmethod
     def get_serializable(cls):
+        print(f' Getting serializable for Dillable')
         from hollarek.abstract.serialization import Dillable
         return Dillable
 
-class TestPicklable(SerializationBaseTest):
+class TestPicklable(HiderClass.SerializationBase):
     @classmethod
     def get_serializable(cls):
         from hollarek.abstract.serialization import Picklable
@@ -121,5 +122,8 @@ class TestPicklable(SerializationBaseTest):
 
 if __name__ == '__main__':
     TestJsonDataclass.execute_all()
+    # dill has a known issue that prevents it from serializing "Enums defined in __main__" in particular
+    # see: https://github.com/uqfoundation/dill/issues/250
+    # Hence if you execute it via __main__ rather than via unittest discovery you will see an error
     TestDillable.execute_all()
     TestPicklable.execute_all()
