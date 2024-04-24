9603fb745321abd8d28218b81463e4cc47583dc9
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-24 22:24:38 +0200

    tests: Restored commented out Countdown tests

--- tests/t_events/t_countdown.py
+++ tests/t_events/t_countdown.py
@@ -30,32 +30,31 @@ class TestCountdown(Unittest):
         logs = sys.stderr.getvalue()
         self.assertNotIn('KeyError',logs)
 
-    #
-    # def test_is_active(self):
-    #     """ Test is_active returns the correct status. """
-    #     self.assertFalse(self.countdown.is_active())
-    #     self.countdown.start()
-    #     self.assertTrue(self.countdown.is_active())
+
+    def test_is_active(self):
+        """ Test is_active returns the correct status. """
+        self.assertFalse(self.countdown.is_active())
+        self.countdown.start()
+        self.assertTrue(self.countdown.is_active())
 
     def test_finish(self):
         """ Test that finish waits for the countdown to complete. """
         self.countdown.start()
         self.countdown.finish()
-        time.sleep(0.0001)
         self.assertFalse(self.countdown.is_active())
 
-    # def test_get_output(self):
-    #     """ Test getting the output after countdown. """
-    #     self.countdown.start()
-    #     self.countdown.finish()  # Ensure countdown has finished
-    #     output = self.countdown.get_output()
-    #     self.assertEqual(output, 'expired')
-    #
-    # def test_expiration_function_called(self):
-    #     """ Test that the on_expiration function is called correctly. """
-    #     self.countdown.start()
-    #     self.countdown.finish()  # Wait for the countdown to finish
-    #     self.on_expiration.assert_called_once()
+    def test_get_output(self):
+        """ Test getting the output after countdown. """
+        self.countdown.start()
+        self.countdown.finish()  # Ensure countdown has finished
+        output = self.countdown.get_output()
+        self.assertEqual(output, 'expired')
+
+    def test_expiration_function_called(self):
+        """ Test that the on_expiration function is called correctly. """
+        self.countdown.start()
+        self.countdown.finish()  # Wait for the countdown to finish
+        self.on_expiration.assert_called_once()
 
     def tearDown(self):
         sys.stderr = default_stderr
