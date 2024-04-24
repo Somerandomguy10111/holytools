b3a625f8f318708d626d59a8f256e3c58d10e384
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-24 21:23:00 +0200

    tests: Removed fail cases in TestMeta

--- tests/t_devtools/t_unit.py
+++ tests/t_devtools/t_unit.py
@@ -7,12 +7,6 @@ class TestMeta(Unittest):
         results = TestOptionalTyping.execute_all()
         self.assertIn(f'tests ran successfully!',results.get_final_status())
 
-    def test_error(self):
-        raise ValueError(f'This bug is tamed and very friendly')
-
-    def test_fail(self):
-        self.fail(msg=f'I failed this test on purposes')
-
 
 if __name__ == "__main__":
     TestMeta.execute_all()
