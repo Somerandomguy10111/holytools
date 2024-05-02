d20c8278c327e81e93a2e707d244147d398d7f16
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-05-02 20:15:46 +0200

    events/Timer: Implemented measure time + tests

--- holytools/events/timer.py
+++ holytools/events/timer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 
 class Timer:
@@ -16,3 +17,13 @@ class Timer:
             print(f'Time has been running for {delta_sec} seconds')
         return delta_sec
 
+    @staticmethod
+    def measure_time(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f"{func.__name__}: Took {end_time - start_time} seconds to execute")
+            return result
+
+        return wrapper
