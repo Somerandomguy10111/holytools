81f54dddfa2bc2eb2b1fd09e82fdd552b12503cc
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-05-25 03:36:49 +0200

    abstract: Updated JsonDataclass to handle list[float] entries

--- holytools/abstract/serialization/jsondataclass.py
+++ holytools/abstract/serialization/jsondataclass.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import ast
+
 import orjson
 import dataclasses
 from datetime import datetime, date, time
@@ -8,7 +10,7 @@ from types import NoneType
 from enum import Enum
 import json
 
-from .serializable import Serializable
+from holytools.abstract.serialization.serializable import Serializable
 # -------------------------------------------
 
 
@@ -23,26 +25,24 @@ class JsonDataclass(Serializable):
         return from_json(cls=cls, json_dict=json_str_dict)
 
     def to_str(self) -> str:
-        json_dict = self.to_json()
+        json_dict = self.to_dict()
         return orjson.dumps(json_dict).decode("utf-8")
 
-    def to_json(self) -> dict:
-        return {attr: get_json_entry(obj=value) for attr, value in self.__dict__.items()}
+    def to_dict(self) -> dict:
+        return {attr: get_jsonifyable_entry(obj=value) for attr, value in self.__dict__.items()}
 
     @classmethod
     def from_json(cls, json_dict : dict) -> JsonDataclass:
         return from_json(cls=cls, json_dict=json_dict)
 
 
-def get_json_entry(obj):
+def get_jsonifyable_entry(obj):
     if isinstance(obj, JsonDataclass):
-        entry = obj.to_json()
+        entry = obj.to_dict()
     elif isinstance(obj, Enum):
         entry = obj.value
     elif hasattr(obj, '__dict__'):
         entry = {attr: value for attr, value in obj.__dict__.items() if not callable(value)}
-    elif isinstance(obj, dict):
-        entry = json.dumps(obj)
     else:
         entry = obj
     return entry
@@ -59,17 +59,19 @@ def from_json(cls : type, json_dict: dict):
             continue
 
         dtype = type_hints.get(key)
-        core_dtype = get_core_type(dtype=dtype)
-        if core_dtype.__name__ in elementary_type_names:
-            init_dict[key] = make_elementary(cls=core_dtype,value=value)
-        elif issubclass(core_dtype, Enum):
-            init_dict[key] = core_dtype(value)
-        elif core_dtype == dict:
+        dtype = strip_nonetype(dtype=dtype)
+        if dtype.__name__ in elementary_type_names:
+            init_dict[key] = make_elementary(cls=dtype,value=value)
+        elif issubclass(dtype, Enum):
+            init_dict[key] = dtype(value)
+        elif get_origin(dtype) == dict:
             init_dict[key] = make_dict(dtype=dtype, value=value)
-        elif dataclasses.is_dataclass(obj=core_dtype):
-            init_dict[key] = from_json(cls=core_dtype, json_dict=value)
+        elif get_origin(dtype) == list:
+            init_dict[key] = value
+        elif dataclasses.is_dataclass(obj=dtype):
+            init_dict[key] = from_json(cls=dtype, json_dict=value)
         else:
-            raise TypeError(f'Unsupported type {core_dtype}')
+            raise TypeError(f'Unsupported type {dtype}')
 
     return cls(**init_dict)
 
@@ -97,7 +99,7 @@ def make_elementary(cls, value : str):
 
 
 def get_dict_item_types(dtype : type) -> (type, type):
-    if not get_core_type(dtype) == dict:
+    if not strip_nonetype(dtype) == dict:
         raise TypeError(f'{dtype} is not a dict')
     args = get_args(dtype)
     if not len(args) == 2:
@@ -107,7 +109,7 @@ def get_dict_item_types(dtype : type) -> (type, type):
 
 
 # noinspection DuplicatedCode
-def get_core_type(dtype : type) -> type:
+def strip_nonetype(dtype : type) -> type:
     origin = get_origin(dtype)
     if origin is Union:
         types = get_args(dtype)
@@ -116,8 +118,6 @@ def get_core_type(dtype : type) -> type:
             core_type = not_none_types[0]
         else:
             raise ValueError(f'Union dtype {dtype} has more than one core dtype')
-    elif origin:
-        core_type = origin
     else:
         core_type = dtype
     return core_type
@@ -130,3 +130,7 @@ conversion_map = {
     time: time.fromisoformat,
 }
 elementary_type_names : list[str] = typecast_classes + [cls.__name__ for cls in conversion_map.keys()]
+
+if __name__ == "__main__":
+    this_dtype = list[float]
+    print(strip_nonetype(this_dtype))
