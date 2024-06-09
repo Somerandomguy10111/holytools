7e013ff7a7ca041ff7051a377f9233939dc6e6a2
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-09 20:08:49 +0200

    tests/abstract: Changed Base test import so that it doesnt get executed on python unittest discovery

--- tests/t_abstract/t_serialization.py
+++ tests/t_abstract/t_serialization.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 from holytools.abstract import SerializableType
-from tests.t_abstract.base import SerializationTest, SimpleDataclass
+# from tests.t_abstract.base import SerializationTest, SimpleDataclass
+#
+import tests.t_abstract.base as base
 
 
-class TestJsonDataclass(SerializationTest):
+class TestJsonDataclass(base.SerializationTest):
     @classmethod
     def get_serializable_type(cls):
         from holytools.abstract.serialization import JsonDataclass
@@ -12,14 +14,14 @@ class TestJsonDataclass(SerializationTest):
 
 # dill has a known issue that prevents it from serializing "Enums defined in __main__" in particular
 # see: https://github.com/uqfoundation/dill/issues/250
-class TestDillable(SerializationTest):
+class TestDillable(base.SerializationTest):
     @classmethod
     def get_serializable_type(cls):
         print(f' Getting serializable for Dillable')
         from holytools.abstract.serialization import Dillable
         return Dillable
 
-class TestPicklable(SerializationTest):
+class TestPicklable(base.SerializationTest):
     @classmethod
     def get_serializable_type(cls):
         from holytools.abstract.serialization import Picklable
@@ -27,14 +29,12 @@ class TestPicklable(SerializationTest):
 
       # pickle is unable to serialize the complex datacalss
     def get_instance_and_cls(self) -> (SerializableType, type[SerializableType]):
-        instance = SimpleDataclass.make_example()
-        cls = SimpleDataclass
+        instance = base.SimpleDataclass.make_example()
+        cls = base.SimpleDataclass
         return instance, cls
 
 
 if __name__ == '__main__':
     TestJsonDataclass.execute_all()
-    # Hence if you execute it via __main__ rather than via unittest discovery you will see an error
-    # TestDillable.execute_all()
     TestPicklable.execute_all()
     TestDillable.execute_all()
