482eb60ca2bf9d99b805566702f476f2b3275b13
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-07-05 11:00:54 +0200

    configs: LocationManager now used as object instead of cls

--- holytools/fsys/location_manager.py
+++ holytools/fsys/location_manager.py
@@ -6,56 +6,32 @@ from typing import Optional
 # -------------------------------------------
 
 class LocationManager:
-    _root_dirpath: Optional[str] = None
-    _dirs: list[str] = []
-
-    def __init__(self):
-        raise ValueError(f'Cannot initialize {self.__class__.__name__}, it is used as a class only')
-
-    @classmethod
-    def is_initialized(cls) -> bool:
-        return cls._root_dirpath is not None
-
-    @classmethod
-    def set_root(cls, root_dirpath: str):
-        if not cls._root_dirpath is None:
-            raise ValueError(f'Root directory already set to {cls._root_dirpath}')
-
-        if root_dirpath is None:
-            raise ValueError(f'Failed to initialize {cls._get_name()}: Root directory is None')
-
+    def __init__(self, root_dirpath : str):
         if not os.path.isdir(root_dirpath):
-            raise ValueError(f'Failed to initialize {cls._get_name()}: Root directory {root_dirpath} does not exist')
+            raise ValueError(f'Failed to initialize {self.__class__.__name__}: Root directory {root_dirpath} does not exist')
+        self._root_dirpath : str = root_dirpath
+        self._subdir_paths: list[str] = []
+        self._fpaths : list[str] = []
 
-        cls._root_dirpath = root_dirpath
-
-    @classmethod
-    def relative_dir(cls, relative_path: str) -> str:
-        dirpath = cls._get_relative_path(relative_path=relative_path)
+    def relative_dir(self, relative_path: str) -> str:
+        dirpath = self._get_relative_path(relative_path=relative_path)
         if not os.path.isdir(dirpath):
-            os.makedirs(dirpath)
-
+            os.makedirs(dirpath, exist_ok=True)
+        self._subdir_paths.append(dirpath)
         return dirpath
 
-    @classmethod
-    def relative_file(cls, relative_path: str) -> str:
-        fpath = cls._get_relative_path(relative_path=relative_path)
+    def relative_file(self, relative_path: str) -> str:
+        fpath = self._get_relative_path(relative_path=relative_path)
+        self._fpaths.append(fpath)
         return fpath
 
     # -------------------------------------------
 
-    @classmethod
-    def _get_relative_path(cls, relative_path: str) -> str:
-        root_dirpath = cls.get_root_dirpath()
+    def _get_relative_path(self, relative_path: str) -> str:
+        root_dirpath = self.get_root_dirpath()
         return os.path.join(root_dirpath, relative_path)
 
-    @classmethod
-    def get_root_dirpath(cls) -> str:
-        if cls._root_dirpath is None:
-            raise ValueError(f'Root directory not set for {cls._get_name()}')
-        return cls._root_dirpath
-
-    @classmethod
-    def _get_name(cls) -> str:
-        return cls.__name__
-
+    def get_root_dirpath(self) -> str:
+        if self._root_dirpath is None:
+            raise ValueError(f'Root directory not set for {self.__class__.__name__}')
+        return self._root_dirpath
