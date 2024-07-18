ad8ec8da971777598006a7135cceeaba27557074
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-07-18 21:42:16 +0200

    logging: Added mute() decorator

--- holytools/logging/__init__.py
+++ holytools/logging/__init__.py
@@ -1,4 +1,6 @@
-
+import os
+import sys
+from typing import Callable
 
 from .factory import Loggable, LoggerFactory
 from .modifier import LoggingModifier
@@ -6,6 +8,18 @@ from .modifier import LoggingModifier
 from datetime import datetime
 def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d_%H%M")
+
+
+def mute(func : Callable) -> Callable:
+    def muted_func(*args, **kwargs):
+        stdout, stderr = sys.stdout, sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+        result = func(*args, **kwargs)
+        sys.stdout, sys.stderr = stdout, stderr
+        return result
+    return muted_func
+
 
 import logging
 class LogLevel:
