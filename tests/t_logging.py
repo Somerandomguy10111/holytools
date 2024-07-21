0d630f22b57c439a04146a564c23aa6841fa1294
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-07-21 19:48:55 +0200

    tests: Updated for make_logger -> get_logger

--- tests/t_logging.py
+++ tests/t_logging.py
@@ -4,18 +4,18 @@ from holytools.devtools import Unittest
 
 class TestLoggin(Unittest):
     def test_info_error(self):
-        my_logger = LoggerFactory.make_logger(name='testlogger')
+        my_logger = LoggerFactory.get_logger(name='testlogger')
         my_logger.info(msg=f'Info text')
         my_logger.error(msg=f'Error text')
 
-    def test_make_logger(self):
+    def test_get_logger(self):
         logger_name = "basic_xyz_logger"
-        _ = LoggerFactory.make_logger(name=logger_name)
+        _ = LoggerFactory.get_logger(name=logger_name)
 
     def test_retrieve_logger(self):
         logger_name = 'this_unique_logger'
 
-        expected_logger = LoggerFactory.make_logger(name=logger_name)
+        expected_logger = LoggerFactory.get_logger(name=logger_name)
         actual_logger = logging.getLogger(name=logger_name)
         self.assertEqual(expected_logger, actual_logger)
         logger_is_registered = logger_name in logging.root.manager.loggerDict
