871a6a36f4f62667eb98346aa89e2267e2be23c8
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-07-21 19:46:34 +0200

    logging: Exported logging tools to dedicated module; Introduced to_sci_notation

--- holytools/logging/__init__.py
+++ holytools/logging/__init__.py
@@ -1,24 +1,6 @@
-import os
-import sys
-from typing import Callable
-
 from .factory import Loggable, LoggerFactory
 from .modifier import LoggingModifier
-
-from datetime import datetime
-def get_timestamp():
-    return datetime.now().strftime("%Y-%m-%d_%H%M")
-
-
-def mute(func : Callable) -> Callable:
-    def muted_func(*args, **kwargs):
-        stdout, stderr = sys.stdout, sys.stderr
-        sys.stdout = open(os.devnull, 'w')
-        sys.stderr = open(os.devnull, 'w')
-        result = func(*args, **kwargs)
-        sys.stdout, sys.stderr = stdout, stderr
-        return result
-    return muted_func
+from .tools import get_timestamp, mute, to_sci_notation
 
 
 import logging
