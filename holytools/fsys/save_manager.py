7f6679b3d12bf87673de34487d854b21c9c8b4de
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-11-25 05:25:14 +0100

    fsys: Renamed tmp_fpath

--- holytools/fsys/save_manager.py
+++ holytools/fsys/save_manager.py
@@ -6,7 +6,7 @@ import tempfile
 
 class SaveManager:
     @staticmethod
-    def tmp_fpath(suffix : str = '') -> str:
+    def get_tmp_fpath(suffix : str = '') -> str:
         return tempfile.mktemp(suffix=suffix)
 
     @staticmethod
