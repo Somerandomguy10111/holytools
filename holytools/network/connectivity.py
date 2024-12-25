73c2b5346d2100aae1550ead2a8599e4f0c4491a
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-12-25 16:20:57 +0100

    network/connectivity: Added package size option

--- holytools/network/connectivity.py
+++ holytools/network/connectivity.py
@@ -7,9 +7,10 @@ import tabulate
 # ----------------------------
 
 class ConnectivityTester:
-    def __init__(self, remote_ip_addr : str = '172.217.12.131', remote_name : str = 'google.de'):
+    def __init__(self, remote_ip_addr : str = '172.217.12.131', remote_name : str = 'google.de', package_size : int = 256):
         self.remote_ip_addr : str = remote_ip_addr
         self.remote_name : str = remote_name
+        self.package_size_bytes : int = package_size
 
     def check_connectivity(self, max_duration : int = 30, verbose : bool = False):
         hosts = [self.remote_ip_addr, self.remote_name]
@@ -24,7 +25,8 @@ class ConnectivityTester:
 
         print(f'Launching connectivity tests for {self.remote_name} = {self.remote_ip_addr}. '
               f'\n  - Duration = {max_duration}s'
-              f'\n  - Timeout = {ping_timeout}s')
+              f'\n  - Timeout = {ping_timeout}s'
+              f'\n  - Package size = {self.package_size_bytes} bytes\n')
         start_time = time.time()
         while time.time() - start_time < max_duration:
             for h in hosts:
@@ -61,13 +63,13 @@ class ConnectivityTester:
         print(tabulate.tabulate(tabular_data=table_data, headers=col_headers, tablefmt='psql'))
         print()
 
-    @staticmethod
-    def is_ping_reachable(timeout : int, host : str) -> bool:
+    def is_ping_reachable(self, timeout : int, host : str) -> bool:
         if timeout < 1:
             raise ValueError('Timeout must be at least 1 second')
 
-        command = f'ping -w {timeout} -c 1 {host}'
+        command = f'ping -w {timeout} -c 1 {host} -s {self.package_size_bytes}'
         return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True) == 0
+
 
 if __name__ == "__main__":
     import argparse
@@ -76,7 +78,11 @@ if __name__ == "__main__":
     parser.add_argument('--duration', type=int)
     args = parser.parse_args()
 
-    tester0 = ConnectivityTester()
+
+    if not args.duration:
+        raise ValueError('Please provide a duration for the connectivity test')
+
+    tester0 = ConnectivityTester(remote_name='chatgpt.com', package_size=512)
     tester0.check_connectivity(max_duration=args.duration, verbose=True)
 
     # duration = 10
