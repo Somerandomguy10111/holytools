5aa90f3ce8a2971fd53f35c6d692759392ed10ae
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-05 18:18:10 +0100

    fsys: Added missing __init__ file import

--- holytools/fsys/__init__.py
+++ holytools/fsys/__init__.py
@@ -1,2 +1,3 @@
 from .location_manager import LocationManager
 from .path_tools import PathTools
+from .fsys_node import FsysNode
--- holytools/fsys/path_tools.py
+++ holytools/fsys/path_tools.py
@@ -1,4 +1,3 @@
-import os
 from typing import Optional
 
 from pathvalidate import sanitize_filename
