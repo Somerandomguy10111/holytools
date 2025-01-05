0f44cf05d039fd7ec22b72aa9bb15a295ccc03a7
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-05 18:23:53 +0100

    tests: Updated fsys tests

--- tests/t_fsys/t_node.py
+++ tests/t_fsys/t_node.py
@@ -1,12 +1,13 @@
-from holytools.devtools import Unittest
-import shutil
 import os
+import shutil
 import tempfile
 
-from tests.t_fsys.t_save_manager import TestSaveManager
+from holytools.devtools import Unittest
+from holytools.fsys import FsysNode
 
+# -------------------------------------------------------------
 
-class FsysTemplate(Unittest):
+class TestFsysNode(Unittest):
     num_hard_files = 9
     num_hard_folders = 3
     num_total_dat_files = 5
@@ -36,30 +37,18 @@ class FsysTemplate(Unittest):
         os.symlink(os.path.join(self.test_dir, 'dir1', 'sub1.dat'), os.path.join(self.test_dir, 'symlink_sub1.dat'))
         self.root_node = FsysNode(self.test_dir)
 
-
-class TestFsyNode(FsysTemplate):
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
-    def test_get_subnodes(self):
-        subnodes = self.root_node.get_subfile_paths()
-        self.assertEqual(len(subnodes), self.num_total_nodes)
-
-    def test_select_dat_files(self):
-        dat_nodes = self.root_node.get_file_subnodes(['.dat'])
-        self.assertEqual(len(dat_nodes), self.num_total_dat_files)
-
     def test_get_file_subnodes(self):
-        file_subnodes = self.root_node.get_file_subnodes()
+        file_subnodes = self.root_node.get_subfile_paths()
         self.assertEqual(len(file_subnodes), self.num_total_files)
 
-    def test_exclude_hidden(self):
-        non_hidden = self.root_node.get_child_nodes(exclude_hidden=True)
-        print(f'Non hidden node are {[node.get_name() for node in non_hidden]}')
-        self.assertEqual(len(non_hidden), 5)
-
+    def test_zip(self):
+        zip_bytes = self.root_node.get_zip()
+        self.assertTrue(len(zip_bytes) > 0)
+        self.assertIsInstance(zip_bytes, bytes)
 
 
 if __name__ == '__main__':
-    # TestFsysNode.execute_all()
-    TestSaveManager.execute_all()
+    TestFsysNode.execute_all()
