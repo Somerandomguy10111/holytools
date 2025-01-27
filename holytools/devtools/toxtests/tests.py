30a6fdc537108bbcb8b7997d0dd6d8af341302d1
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-27 15:14:17 +0100

    devtools: Introduced tests() logic to toxtests

--- holytools/devtools/toxtests/tests.py
+++ holytools/devtools/toxtests/tests.py
@@ -1,19 +1,27 @@
 import os
+import sys
+
 from tox.run import run
 
+# -------------------------------------------------------------
 
 def main():
-    mode = 'pkg'
-    conditional_recreate = ''
-    script_dirpath = os.path.dirname(__file__)
     cwd = os.getcwd()
     os.environ['REPO_DIRPATH'] = cwd
     os.environ['TOX_ENVNAME'] = os.path.basename(cwd)
-    args = ('-c', os.path.join(script_dirpath, 'tox.ini'), '-e', mode)
-    if conditional_recreate:
-        args = (*args,conditional_recreate)
+    mode = 'pkg' if is_package(dirpath=cwd) else 'req'
+    script_dirpath = os.path.dirname(__file__)
+    tox_fpath = os.path.join(script_dirpath, 'tox.ini')
 
+    args = ('-c', tox_fpath , '-e', mode)
+    if len(sys.argv) > 1:
+        args = (*args,sys.argv[1])
     run(args)
 
-if __name__ == "__main__":
-    main()
+
+def is_package(dirpath : str):
+    fnames = os.listdir(dirpath)
+    has_setup = 'setup.py' in fnames
+    has_pyproject = 'pyproject.toml' in fnames
+    return has_setup or has_pyproject
+
