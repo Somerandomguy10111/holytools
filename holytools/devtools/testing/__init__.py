1175302532144491995f7aa738bfab433afff69d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-28 05:38:23 +0100

    General: Removed toxlauncher from holytools

--- holytools/devtools/testing/__init__.py
+++ holytools/devtools/testing/__init__.py
@@ -4,27 +4,3 @@ from .unit import Unittest
 import os
 import sys
 
-from tox.run import run
-
-# -------------------------------------------------------------
-
-def toxtests():
-    cwd = os.getcwd()
-    os.environ['REPO_DIRPATH'] = cwd
-    os.environ['TOX_ENVNAME'] = os.path.basename(cwd)
-    mode = 'pkg' if is_package(dirpath=cwd) else 'req'
-    script_dirpath = os.path.dirname(__file__)
-    tox_fpath = os.path.join(script_dirpath, 'tox.ini')
-
-    args = ('-c', tox_fpath , '-e', mode)
-    if len(sys.argv) > 1:
-        args = (*args,sys.argv[1])
-    run(args)
-
-
-def is_package(dirpath : str):
-    fnames = os.listdir(dirpath)
-    has_setup = 'setup.py' in fnames
-    has_pyproject = 'pyproject.toml' in fnames
-    return has_setup or has_pyproject
-
