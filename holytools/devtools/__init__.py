ac1498eb74c9c77efcc16f76d2af2914d8f999bf
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-03-01 19:58:01 +0100

    General: Removed profiler dependencies

--- holytools/devtools/__init__.py
+++ holytools/devtools/__init__.py
@@ -1,6 +1,5 @@
 from .testing import Unittest
 from .inspection import ModuleInspector, TypeInspector, Argument
-from .profiling import ProfiledImportScope, Profiler
 from .input import InputSimulator
 
 
