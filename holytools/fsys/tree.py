9b22cc9b4556de30012a3dd1c1768186d8d2e4d5
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-05-16 14:21:03 +0200

    fsys: Consolidtaed dict_to_tree arguments

--- holytools/fsys/tree.py
+++ holytools/fsys/tree.py
@@ -1,21 +1,20 @@
 import os
+from dataclasses import dataclass, field
 from typing import Optional
+
+@dataclass
+class TreeDecorator:
+    desc_map : dict[str, str] = field(default_factory=dict)
+    path_to_fileID : dict[str, int] = field(default_factory=dict)
 
 
 class TreeGenerator:
     @classmethod
-    def dict_to_tree(cls, fsys_dict: dict, desc_map : Optional[dict[str, str]] = None, path_to_fileID : Optional[dict[str, int]] = None,
-                     indent: int = 0, parent_dirpath : str = '/', max_children: int = 10) -> str:
-        if desc_map is None:
-            desc_map = {}
-        if path_to_fileID is None:
-            path_to_fileID = {}
-
+    def dict_to_tree(cls, fsys_dict: dict, decorator : Optional[TreeDecorator] = None, max_children: int = 10,
+                          indent: int = 0, parent_dirpath : str = '/') -> str:
         total_str = ''
-        if not fsys_dict:
-            return total_str
-
         items = fsys_dict.items()
+        desc_map, path_to_fileID = decorator.desc_map, decorator.path_to_fileID
         for n, (k, v) in enumerate(items):
             indentation = '\t' * indent
 
@@ -25,14 +24,15 @@ class TreeGenerator:
 
             fpath = os.path.join(f'{parent_dirpath}', k)
             is_file = os.path.isfile(fpath)
-            symbol = f'🗎' if is_file else '🗀'
-            conditional_backslash = '' if is_file else '/'
-            conditional_description = f'{indentation}{desc_map[fpath]}\n' if fpath in desc_map else ''
-            conditional_fileID = f' | FileID = {path_to_fileID[fpath]}' if fpath in path_to_fileID else ''
 
-            total_str += (f'{indentation}{symbol} {k}{conditional_backslash}{conditional_fileID}\n'
-                          f'{conditional_description}'
-                          f'{cls.dict_to_tree(v, indent=indent + 1, parent_dirpath=fpath, desc_map=desc_map, path_to_fileID=path_to_fileID)}')
+            symbol = f'🗎' if is_file else '🗀'
+            cond_backslash = '' if is_file else '/'
+            cond_desc = f'{indentation}{desc_map[fpath]}\n' if fpath in desc_map else ''
+            cond_fileID = f' | FileID = {path_to_fileID[fpath]}' if fpath in path_to_fileID else ''
+
+            total_str += (f'{indentation}{symbol} {k}{cond_backslash}{cond_fileID}\n'
+                          f'{cond_desc}'
+                          f'{cls.dict_to_tree(v, indent=indent + 1, parent_dirpath=fpath, decorator=decorator)}')
         if indent == 0:
             total_str = total_str.rstrip()
         return total_str
