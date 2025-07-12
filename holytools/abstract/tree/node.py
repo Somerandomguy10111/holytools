47c2f0690a7ec42cf494c2a2aea17a569da82606
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-07-12 14:09:00 +0200

    abtract: Streamlined tree node

--- holytools/abstract/tree/node.py
+++ holytools/abstract/tree/node.py
@@ -1,44 +1,71 @@
 from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Optional, Callable
+
 from typing import Optional
 from abc import abstractmethod, ABC
 
 # -------------------------------------------
 
 
-class TreeNode(ABC):
-    def get_subnodes(self, *args, **kwargs) -> list[TreeNode]:
-        _, __ = args, kwargs
-        subnodes = []
-        for child in self.get_child_nodes():
-            subnodes.append(child)
-            subnodes += child.get_subnodes()
-        return subnodes
+tree_str = """root
+    a
+        a
+        a
+        b
+            c
+    b
+        a
+            a
+        a
+        d"""
 
-    def get_ancestors(self) -> list[TreeNode]:
-        current = self
-        ancestors = []
-        while current.get_parent():
-            ancestors.append(current.get_parent())
-            current = current.get_parent()
-        return ancestors
 
-    def get_root(self) -> TreeNode:
-        current = self
-        while current.get_parent():
-            current = current.get_parent()
-        return current
+@dataclass
+class Node:
+    def __init__(self, name : str):
+        self.name : str = name
+        self.children : list[Node] = []
 
-    @abstractmethod
-    def get_name(self) -> str:
-        pass
+    def get_descendants(self):
+        desc : list[Node] = [x for x in self.children]
+        for c in self.children:
+            desc += c.get_descendants()
+        return desc
 
-    @abstractmethod
-    def get_child_nodes(self, *args, **kwargs) -> list[TreeNode]:
-        pass
+    @classmethod
+    def from_str(cls, s : str) -> Node:
+        lines = s.split('\n')
 
-    @abstractmethod
-    def get_parent(self) -> Optional[TreeNode]:
-        pass
+        root = Node(name=lines[0])
+        ancestors : list[Node] = [root]
 
-    def __str__(self):
-        return self.get_name()
+        for l in lines[1:]:
+            sl = l.lstrip(' ')
+            blank_lines = len(l) - len(sl)
+            if not blank_lines % 4 == 0:
+                raise ValueError(f'Invalid indentation in line: {l}. Must be multiple of 4')
+
+            indent = blank_lines // 4
+            if indent > len(ancestors):
+                raise ValueError(f'Invalid indentation in line: {l}')
+
+            while indent < len(ancestors):
+                ancestors.pop()
+
+            node = Node(name=sl)
+            parent = ancestors[-1]
+            parent.children.append(node)
+            ancestors.append(node)
+
+        return root
+
+    def get_tree(self, indent : int   = 0):
+        tree = '|\t' * indent +  self.name
+        for c in self.children:
+            tree += f'\n{c.get_tree(indent=indent+1)}'
+        return tree
+
+
+example_node = Node.from_str(s=tree_str)
+print(example_node.get_tree())
