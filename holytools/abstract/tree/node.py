ad04df7827d6af0280086f11b18027c4467ec8b6
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-07-21 12:04:05 +0200

    tests: Added TreeNode tests

--- holytools/abstract/tree/node.py
+++ holytools/abstract/tree/node.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import copy
 from dataclasses import dataclass
-from typing import Callable, Optional, Any
+from typing import Callable, Optional, Any, Self
+
+from cryptography.utils import cached_property
 
 
 # -------------------------------------------
@@ -34,8 +36,8 @@ class TreeNode:
             while indent < len(ancestors):
                 ancestors.pop()
 
-            parent = ancestors[-1]
             node = TreeNode(name=sl)
+            parent = ancestors[-1]
             parent.children.append(node)
             ancestors.append(node)
 
@@ -44,21 +46,20 @@ class TreeNode:
     # -------------------------------------------
     # Tree manipulation
 
-    def create_pruned_subtree[T](self : T | TreeNode, is_relevant : Callable[[T | TreeNode], bool]) -> list[T]:
-        pruned_children = []
-        for c in self.children:
-            pruned_children += c.create_pruned_subtree(is_relevant=is_relevant)
+    def create_pruned(self, is_relevant : Callable[[Self], bool]) -> Optional[Self]:
+        new_children = [c.create_pruned(is_relevant=is_relevant) for c in self.children]
+        new_children = [c for c in new_children if not c is None]
+        is_included = is_relevant(self) or len(new_children) > 0
 
-        if is_relevant(self):
-            new = self.mk_empty_copy()
-            new.children = pruned_children
-            return [new]
-        else:
-            return pruned_children
+        new = self.mk_empty_copy() if is_included else None
+        if new:
+            new.children = new_children
+        return new
 
-    def create_unique_subtree(self, node_map : Optional[dict[str, TreeNode]] = None, parent : Optional[TreeNode] = None) -> TreeNode:
+    def create_unique(self, node_map : Optional[dict[str, TreeNode]] = None, parent : Optional[TreeNode] = None) -> TreeNode:
         if node_map is None:
             node_map = {}
+
         uid = self.get_id()
         if not uid in node_map:
             new = self.mk_empty_copy()
@@ -67,12 +68,8 @@ class TreeNode:
                 node_map[parent.get_id()].children.append(new)
 
         for c in self.children:
-            c.create_unique_subtree(node_map=node_map, parent=node_map[uid])
-
+            c.create_unique(node_map=node_map, parent=node_map[uid])
         return node_map[uid]
-
-    def get_id(self) -> str:
-        return self.name
 
     def mk_empty_copy(self) -> TreeNode:
         new = copy.deepcopy(self)
@@ -83,15 +80,18 @@ class TreeNode:
     # ------------------------------------------------
     # Navigation
 
-    def get_node_to_idx(self) -> dict[str, int]:
+    def get_nodeid_to_idx(self) -> dict[str, int]:
         idx_to_node = self.get_idx_to_node()
         return {node.get_id() : j for j, node in idx_to_node.items()}
 
-    def get_idx_to_node[T](self : T | TreeNode) -> dict[int, T]:
+    def get_idx_to_node(self) -> dict[int, Self]:
         all_nodes = [self] + self.get_descendants()
         all_nodes = [x for x in all_nodes if x.is_indexable()]
 
         return {j : node for j, node in enumerate(all_nodes)}
+
+    def get_id(self) -> str:
+        return self.name
 
     def is_indexable(self) -> bool:
         _ = self
@@ -107,8 +107,8 @@ class TreeNode:
     # -----------------------------------------------------
     # Properties
 
-    def get_tree(self, indent : int   = 0, node_to_idx : Optional[dict[str, int]] = None):
-        idx = node_to_idx.get(self.get_id()) if not node_to_idx is None else None
+    def get_tree(self, indent : int   = 0, nodeid_to_idx : Optional[dict[str, int]] = None):
+        idx = nodeid_to_idx.get(self.get_id()) if not nodeid_to_idx is None else None
         desc = self.get_desc()
 
         marked_indent = '|\t' * indent
@@ -119,7 +119,7 @@ class TreeNode:
                 f'{conditional_desc}')
 
         for c in self.children:
-            tree += f'\n{c.get_tree(indent=indent+1, node_to_idx=node_to_idx)}'
+            tree += f'\n{c.get_tree(indent=indent+1, nodeid_to_idx=nodeid_to_idx)}'
         return tree
 
     def get_desc(self) -> str:
@@ -128,20 +128,3 @@ class TreeNode:
 
     def get_fullname(self) -> str:
         return self.name
-
-if __name__ == "__main__":
-    nm = {}
-    tree_str = """a
-    b
-        a
-        c
-            b
-            b2
-    c
-        a3"""
-
-    example_node = TreeNode.from_str(s=tree_str)
-    unique_node = example_node.create_unique_subtree()
-    print(unique_node.get_tree())
-    example_idx_to_node = unique_node.get_node_to_idx()
-    print(unique_node.get_tree(node_to_idx=example_idx_to_node))
--- tests/t_abstract/t_tree.py
+++ tests/t_abstract/t_tree.py
@@ -1,4 +1,41 @@
+from holytools.abstract import TreeNode
 from holytools.devtools import Unittest
 
-class TestTreeNodeMerge(Unittest):
-    pass
+class TestTreeNode(Unittest):
+    def setUp(self):
+        self.tree_str = """root
+    child1
+        grandchild1
+        grandchild2
+        child2
+    child2"""
+
+    def test_from_str(self):
+        tree = TreeNode.from_str(self.tree_str)
+        self.assertEqual(tree.name, "root")
+        self.assertEqual(len(tree.children), 2)
+        self.assertEqual(tree.children[0].name, "child1")
+        self.assertEqual(len(tree.children[0].children), 3)
+        self.assertEqual(tree.children[1].name, "child2")
+
+    def test_create_pruned(self):
+        def is_relevant_fn(node):
+            return node.name == 'child2' or node.name == 'grandchild2'
+
+        root = TreeNode.from_str(self.tree_str)
+        pruned = root.create_pruned(is_relevant_fn)
+
+        tree = pruned.get_tree()
+        print(f'-Pruned subtree: {tree}')
+        self.assertEqual(pruned.name, "root")
+        self.assertIn('|	|	grandchild2', tree)
+        self.assertIn('|	child2', tree)
+        self.assertNotIn('grandchild1', tree)
+
+    def test_create_unique(self):
+        root = TreeNode.from_str(self.tree_str)
+        unique_tree = root.create_unique()
+        print(f'-Unique tree: {unique_tree.get_tree()}')
+
+if __name__ == "__main__":
+    TestTreeNode.execute_all()
